3eefcbb165bb40213ee484a5261ab9a93001281a
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2022-03-26 23:59:08 +0300

    Initial commit

---
+++ utils/enums.js
@@ -0,0 +1,10 @@
+/* Пример замечания:
+  Можно лучше: Сейчас у вас в наименованиях используются разные подходы. 
+  Было бы лучше, если вы бы выбрали единый.
+*/
+const PageEnum = {
+  SiteWrapper: {
+    SEARCH: document.querySelector(`.columns__search-item`),
+    rating: document.querySelector(`.columns__rating`)
+  }
+};
